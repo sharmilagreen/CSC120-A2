c0f4a28d85bfc926a51dbffdb9411becce575a4b
sharmilagreen <sagreen@smith.edu>
2025-02-27 11:51:44 -0500

    Wrote the code

--- computer.py
+++ computer.py
@@ -1,10 +1,41 @@
 class Computer:
 
     # What attributes will it need?
-
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
+    def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: str, price: int):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
     # What methods will you need?
+
+    def changePrice(self, newPrice:int):
+        self.price = newPrice
+
+    def changeOS(self, newOS:int):
+        self.operating_system = newOS
+    
+    def refurbish(self):
+        if self.year_made < 2000:
+            self.price = 0
+        elif self.year_made < 2012:
+            self.price = 250
+        elif self.year_made < 2018:
+            self.price = 550
+        else:
+            self.price = 1000
+
+
+    
+    
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,23 @@
 class ResaleShop:
+    inventory: list = []
 
     # What attributes will it need?
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+        self.inventory = []
+    
+    def buy(self, computer):
+        self.inventory.append(computer)
+        return computer
+    
+    def sell(self, computer):
+        self.inventory.remove(computer)
+        return True
+    
+
+
+
 
     # What methods will you need?
